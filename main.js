ee90f1915776d9dd0203b9ad6d4b5824326c0333
Lawal Lanre <larrystonegroup@gmail.com>
2017-05-08 22:27:32 +0100

    write getPrimes() function code

--- main.js
+++ main.js
@@ -2,10 +2,24 @@
 
 module.exports = {
 
- /* list prime numbers from 0 to number */
- getPrime: (number) => {
-   let primes = 2
-   // code here
+ /* list prime numbers from 0 to n */
+ getPrimes: (n) => {
+   let primes = [];
+
+   if (n == NaN || n < 2)
+      return primes;
+
+  iteration: for (let k = 2; k <= n; k++) {
+   let half = Math.floor(Math.sqrt(k));
+
+   for (let i = 2; i <= half; i++) {
+      if (k % i == 0) {
+         continue iteration;
+      }
+   }
+   primes.push(k);
+  }
+   
    return primes;
  },
 }
--- tests/tests.js
+++ tests/tests.js
@@ -6,7 +6,7 @@ const assert = require('chai').assert;
 const myApp = require('../main.js');
 
 describe("Get Prime Numbers", function() {
-  it("should return [2,3] for getPrime(4)", function() {
-    assert.equal(myApp.getPrime(4), [2,3]);
+  it("should return [2,3] for getPrimes(4)", function() {
+    assert.equal(myApp.getPrimes(4).toString(), [2,3].toString());
   })
 })
