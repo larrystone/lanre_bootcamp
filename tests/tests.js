b1d7d0818bcc1f33024d6f8cda1bf27145f9b248
Lawal Lanre <larrystonegroup@gmail.com>
2017-05-08 22:45:19 +0100

    create test suite for edge cases of test

--- tests/tests.js
+++ tests/tests.js
@@ -10,8 +10,29 @@ describe("Get Prime Numbers", function() {
     assert.equal(myApp.getPrimes(4).toString(), [2,3].toString());
   })
 
-  it("should return [2,3, 5] for getPrimes(6)", function() {
+  it("should return [2,3,5] for getPrimes(6)", function() {
     assert.equal(myApp.getPrimes(6).toString(), [2,3, 5].toString());
   })
+})
 
+describe("Get Prime Numbers for edge cases", function() {
+  it("should return [] for getPrimes(1)", function() {
+    assert.equal(myApp.getPrimes(1).toString(), [].toString());
+  })  
+
+  it("should return [] for getPrimes(0)", function() {
+    assert.equal(myApp.getPrimes(0).toString(), [].toString());
+  })
+
+  it("should return [] for getPrimes(-1)", function() {
+    assert.equal(myApp.getPrimes(-1).toString(), [].toString());
+  })
+
+  it("should return [] for getPrimes(-8)", function() {
+    assert.equal(myApp.getPrimes(-8).toString(), [].toString());
+  })
+
+  it("should return [] for getPrimes(-100)", function() {
+    assert.equal(myApp.getPrimes(-100).toString(), [].toString());
+  })
 })
